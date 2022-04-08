1baf8e1cda4cac519bb04c601ab4193fc01a864f
donhyuck <leedonhyuck123@gmail.com>
2022-04-09 01:44:21 +0900

    연습 시작

---
+++ Greedy/src/Joystick/Solution.java
@@ -0,0 +1,15 @@
+package Joystick;
+
+class Solution {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int num = solution.solution("name");
+		System.out.println(num);
+	}
+
+	public int solution(String name) {
+		int answer = 0;
+		return answer;
+	}
+}
