7368fe08c622217d4b40c8b64d8f2d0dda47700c
donhyuck <leedonhyuck123@gmail.com>
2022-04-12 23:29:49 +0900

    DFS_BFS 타겟넘버 문제 풀이 시작

---
+++ 코딩테스트 고득점 Kit/src/DFS_BFS/타겟넘버/Solution.java
@@ -0,0 +1,22 @@
+package DFS_BFS.Å¸°Ù³Ñ¹ö;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Solution test = new Solution();
+
+		int[] numbers = new int[] { 1, 1, 1, 1, 1 };
+		int target = 3;
+
+		int a = test.solution(numbers, target);
+
+		System.out.println(a);
+
+	}
+
+	public int solution(int[] numbers, int target) {
+		int answer = 0;
+		return answer;
+	}
+}
