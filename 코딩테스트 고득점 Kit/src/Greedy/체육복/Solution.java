e9944571f21358070c039e1ad1b0dc9bacad2ecf
donhyuck <leedonhyuck123@gmail.com>
2022-04-12 19:22:59 +0900

    체육복 문제 풀이

--- 코딩테스트 고득점 Kit/src/Greedy/체육복/Solution.java
+++ 코딩테스트 고득점 Kit/src/Greedy/체육복/Solution.java
@@ -15,4 +15,59 @@ public class Solution {
 		System.out.println(a);
 
 	}
+
+	public int solution(int n, int[] lost, int[] reserve) {
+
+		int answer = 0;
+		int[] total = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			total[i] = 1;
+		}
+
+		for (int l : lost) {
+			total[l]--;
+		}
+
+		for (int r : reserve) {
+			total[r]++;
+		}
+
+		if (total[1] == 0 && total[2] == 2) {
+			total[1] = 1;
+			total[2] = 1;
+		}
+
+		if (total[n - 1] == 2 && total[n] == 0) {
+			total[n - 1] = 1;
+			total[n] = 1;
+		}
+
+		for (int i = 2; i < n; i++) {
+			if (total[i] == 0) {
+				if (total[i - 1] == 2) {
+					total[i] = 1;
+					total[i - 1] = 1;
+				}
+
+				else if (total[i + 1] == 2) {
+					total[i] = 1;
+					total[i + 1] = 1;
+				}
+			}
+		}
+
+		for (int i = 1; i <= n; i++) {
+			System.out.printf("%d ", total[i]);
+		}
+
+		for (int i = 1; i <= n; i++) {
+			if (total[i] >= 1) {
+				answer++;
+			}
+		}
+
+		return answer;
+
+	}
 }
