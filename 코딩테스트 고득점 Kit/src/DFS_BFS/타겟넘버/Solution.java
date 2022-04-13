81d9cfc82eb4d3a37dcc4cda7e03aabd2760603d
donhyuck <leedonhyuck123@gmail.com>
2022-04-13 17:53:08 +0900

    DFS_BFS 타겟넘버 문제 풀이완료

--- 코딩테스트 고득점 Kit/src/DFS_BFS/타겟넘버/Solution.java
+++ 코딩테스트 고득점 Kit/src/DFS_BFS/타겟넘버/Solution.java
@@ -5,18 +5,44 @@ public class Solution {
 	public static void main(String[] args) {
 
 		Solution test = new Solution();
+		int[] numbers;
+		int target;
+		int result;
 
-		int[] numbers = new int[] { 1, 1, 1, 1, 1 };
-		int target = 3;
+		// 테스트 1
+		numbers = new int[] { 1, 1, 1, 1, 1 };
+		target = 3;
+		result = test.solution(numbers, target);
+		System.out.println("Test 1 : " + result);
 
-		int a = test.solution(numbers, target);
-
-		System.out.println(a);
-
+		// 테스트 2
+		numbers = new int[] { 4, 1, 2, 1 };
+		target = 4;
+		result = test.solution(numbers, target);
+		System.out.println("Test 2 : " + result);
 	}
 
 	public int solution(int[] numbers, int target) {
-		int answer = 0;
+
+		int answer = dfs(numbers, 0, target, 0);
+
 		return answer;
+	}
+
+	public int dfs(int[] numbers, int depth, int target, int sum) {
+
+		if (depth == numbers.length) {
+			if (target == sum) {
+				return 1;
+			}
+
+			return 0;
+		}
+
+		else {
+			return dfs(numbers, depth + 1, target, sum + numbers[depth])
+					+ dfs(numbers, depth + 1, target, sum - numbers[depth]);
+		}
+
 	}
 }
