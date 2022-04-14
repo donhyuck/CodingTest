3f06e08814237d961d086107b639cf7e5083ab58
donhyuck <leedonhyuck123@gmail.com>
2022-04-15 00:29:05 +0900

    해시 - 완주하지 못한 선수 테스트 데이터 설정

--- 코딩테스트 고득점 Kit/src/해시/완주하지못한선수/Solution.java
+++ 코딩테스트 고득점 Kit/src/해시/완주하지못한선수/Solution.java
@@ -1,6 +1,33 @@
 package 해시.완주하지못한선수;
 
 class Solution {
+	public static void main(String[] args) {
+
+		Solution test = new Solution();
+		String runner;
+		String[] participant;
+		String[] completion;
+
+		// 테스트1
+		participant = new String[] { "leo", "kiki", "eden" };
+		completion = new String[] { "eden", "kiki" };
+		runner = test.solution(participant, completion);
+		System.out.println("Test 1 : " + runner);
+
+		// 테스트2
+		participant = new String[] { "marina", "josipa", "nikola", "vinko", "filipa" };
+		completion = new String[] { "josipa", "filipa", "marina", "nikola" };
+		runner = test.solution(participant, completion);
+		System.out.println("Test 2 : " + runner);
+
+		// 테스트3
+		participant = new String[] { "mislav", "stanko", "mislav", "ana" };
+		completion = new String[] { "stanko", "ana", "mislav" };
+		runner = test.solution(participant, completion);
+		System.out.println("Test 3 : " + runner);
+
+	}
+
 	public String solution(String[] participant, String[] completion) {
 		String answer = "";
 		return answer;
